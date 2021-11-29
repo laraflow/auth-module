7f304468b3c80c6bdf5627ad0c41e887e6ae4fb4
Hafijul Islam <hafijul233@gmail.com>
2021-11-29 18:40:01 +0600

    Auth, Admin, Dashboard, Rbac, Core Stabled Inner check needed

--- Routes/web.php
+++ Routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Auth\Http\Controllers\AuthenticatedSessionController;
+use Modules\Auth\Http\Controllers\ConfirmablePasswordController;
+use Modules\Auth\Http\Controllers\EmailVerificationNotificationController;
+use Modules\Auth\Http\Controllers\EmailVerificationPromptController;
+use Modules\Auth\Http\Controllers\NewPasswordController;
+use Modules\Auth\Http\Controllers\PasswordResetLinkController;
+use Modules\Auth\Http\Controllers\RegisteredUserController;
+use Modules\Auth\Http\Controllers\VerifyEmailController;
 
 /*
 |--------------------------------------------------------------------------
